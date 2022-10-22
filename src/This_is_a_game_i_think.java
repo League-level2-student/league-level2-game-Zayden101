857c71369a43c059fd7df4e5d79600cf0f0a5a46
league <league@imac-24.attlocal.net>
2022-10-21 17:01:27 -0700

    I have absolutely what im going to do

---
+++ src/This_is_a_game_i_think.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class This_is_a_game_i_think {
+
+	public static void main(String[] args) {
+		
+
+
+System.out.println("welcome to this is a game i think, i have absolutely what im going to do");
+
+
+int move = JOptionPane.showOptionDialog(null, "hi", "hello", 0, JOptionPane.INFORMATION_MESSAGE, null,
+new String[] { "option 1", "option 2" }, null); 
+
+	}
+
+}
