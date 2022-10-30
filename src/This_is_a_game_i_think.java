1f5548e4de880edf14c11ea1b4852660a82b4b36
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2022-10-30 12:56:31 -0700

    idk what im going to do for the game

--- src/This_is_a_game_i_think.java
+++ src/This_is_a_game_i_think.java
@@ -1,17 +1,153 @@
-import javax.swing.JOptionPane;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Panel;
 
-public class This_is_a_game_i_think {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class This_is_a_game_i_think{
 
 	public static void main(String[] args) {
 		
 
-
 System.out.println("welcome to this is a game i think, i have absolutely what im going to do");
 
+/*
+JPanel mp = new JPanel();
+JFrame mf = new JFrame();
+JButton m1 = new JButton();
+JButton m2 = new JButton();
+JButton m3 = new JButton();
+JButton m4 = new JButton();
 
-int move = JOptionPane.showOptionDialog(null, "hi", "hello", 0, JOptionPane.INFORMATION_MESSAGE, null,
-new String[] { "option 1", "option 2" }, null); 
+mp.add(m1);
+mp.add(m2);
+mp.add(m3);
+mp.add(m4);
+
+m1.setText("Up");
+m2.setText("Down");
+m3.setText("Right");
+m4.setText("left");
+
+
+mf.add(mp);
+mf.pack();
+mf.show();
+*/
+
+
+JButton s1 = new JButton();
+JButton s2 = new JButton();
+JButton s3 = new JButton();
+JButton s4 = new JButton();
+JButton s5 = new JButton();
+JButton s6 = new JButton();
+JButton s7 = new JButton();
+JButton s8 = new JButton();
+JButton s9 = new JButton();
+JButton s10 = new JButton();
+JButton s11 = new JButton();
+JButton s12 = new JButton();
+JButton s13 = new JButton();
+JButton s14 = new JButton();
+JButton s15 = new JButton();
+JButton s16 = new JButton();
+JButton s17 = new JButton();
+JButton s18 = new JButton();
+JButton s19 = new JButton();
+JButton s20 = new JButton();
+JButton s21 = new JButton();
+JButton s22 = new JButton();
+JButton s23 = new JButton();
+JButton s24 = new JButton();
+JButton s25 = new JButton();
+
+JPanel sp = new JPanel();
+JFrame sf = new JFrame();
+
+sp.add(s1);
+sp.add(s2);
+sp.add(s3);
+sp.add(s4);
+sp.add(s5);
+sp.add(s6);
+sp.add(s7);
+sp.add(s8);
+sp.add(s9);
+sp.add(s10);
+sp.add(s11);
+sp.add(s12);
+sp.add(s13);
+sp.add(s14);
+sp.add(s15);
+sp.add(s16);
+sp.add(s17);
+sp.add(s18);
+sp.add(s19);
+sp.add(s20);
+sp.add(s21);
+sp.add(s22);
+sp.add(s23);
+sp.add(s24);
+sp.add(s25);
+
+s1.setText("-");
+s2.setText("-");
+s3.setText("-");
+s4.setText("-");
+s5.setText("-");
+s6.setText("-");
+s7.setText("-");
+s8.setText("-");
+s9.setText("-");
+s10.setText("-");
+s11.setText("-");
+s12.setText("-");
+s13.setText("-");
+s14.setText("-");
+s15.setText("-");
+s16.setText("-");
+s17.setText("-");
+s18.setText("-");
+s19.setText("-");
+s20.setText("-");
+s21.setText("-");
+s22.setText("-");
+s23.setText("-");
+s24.setText("-");
+s25.setText("-");
+
+
+
+//sf.add(sp, BorderLayout.NORTH);
+
+
+
+sf.add(sp);
+sf.pack();
+sf.show();
+
+
+
+
+
+
+
+
+
+int move = JOptionPane.showOptionDialog(null, "Move", "hello", 0, JOptionPane.INFORMATION_MESSAGE, null,
+new String[] { "Right", "Left","Up", "Down" }, null); 
+
+if (move==0) {
+
+}
+
 
 	}
+
+
 
 }
